e8c64bb2cf079aae73c07d5d38ba5346852fd4e3
KenshiroKosuke <mahasamud555@gmail.com>
2023-07-12 11:23:38 +0700

    feat(backend): auth and error

---
+++ apps/backend/src/auth/auth.controller.ts
@@ -0,0 +1,117 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  JWTPayload,
+  JWTToken,
+  LoginRequest,
+  RegisterUserResponse,
+  RegisterUserRetailRequest,
+} from 'types';
+import { User } from './user.decorator';
+import { AuthService } from './auth.service';
+import { JwtRefreshAuthGuard } from './jwt-refresh-auth.guard';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AllExceptionsFilter } from 'src/common/exception.filter';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import * as database from 'database';
+
+@ApiTags('auth')
+@Controller('auth')
+@UseFilters(AllExceptionsFilter)
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Data already exists in another user.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request. Please check your input again.',
+  })
+  @Post('register/retail')
+  @HttpCode(HttpStatus.CREATED)
+  async registerRetail(
+    @Body() userData: RegisterUserRetailRequest,
+  ): Promise<RegisterUserResponse> {
+    return await this.authService.registerRetail(userData);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Data already exists in another user.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request. Please check your input again.',
+  })
+  @Post('register/SMEs')
+  @HttpCode(HttpStatus.CREATED)
+  async registerSMEs(
+    @Body() userData: RegisterUserRetailRequest,
+  ): Promise<RegisterUserResponse> {
+    return await this.authService.registerSMEs(userData);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The user has successfully logged in.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request. Please check your input again.',
+  })
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async logIn(@Body() userData: LoginRequest): Promise<JWTToken> {
+    return await this.authService.logIn(userData);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Token has been refreshed.',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized.',
+  })
+  @ApiBearerAuth()
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtRefreshAuthGuard)
+  async refresh(@User() user: JWTPayload): Promise<JWTToken> {
+    return await this.authService.generateToken({ userID: user.userId });
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Token has been refreshed.',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized.',
+  })
+  @ApiBearerAuth()
+  @Get('profile')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  async profile(@User() user: JWTPayload): Promise<database.User> {
+    return await this.authService.profile(user.userID);
+  }
+}
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { backendConfig } from 'config';
-import { JWTPayload, JWTToken } from 'types';
+import * as bcrypt from 'bcrypt';
+import {
+  JWTPayload,
+  JWTToken,
+  LoginRequest,
+  RegisterUserResponse,
+  RegisterUserRetailRequest,
+} from 'types';
+import { UsersRepository } from '../repositories/userRepo';
+import { InvalidRequestError, PermissionError } from 'src/common/error';
+import { User } from 'database';
 
 @Injectable()
 export class AuthService {
+  private readonly hashRound: number = backendConfig.bcrypt.hashRound;
   private readonly jwtAccessTokenOptions = {
     secret: backendConfig.jwt.accessToken.secret,
     expiresIn: backendConfig.jwt.accessToken.expire,
@@ -14,7 +25,51 @@ export class AuthService {
     expiresIn: backendConfig.jwt.refreshToken.expire,
   };
 
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private readonly usersRepo: UsersRepository,
+  ) {}
+
+  async registerSMEs(
+    userData: RegisterUserRetailRequest,
+  ): Promise<RegisterUserResponse> {
+    const { password, ...data } = userData;
+    const hashedPassword = await bcrypt.hash(password, this.hashRound);
+    const repoResult = await this.usersRepo.createUser({
+      password: hashedPassword,
+      ...data,
+    });
+    return {
+      email: repoResult.email,
+      id: repoResult.id,
+      name: repoResult.name,
+      phone: repoResult.phone,
+      role: repoResult.role,
+    };
+  }
+
+  async registerRetail(
+    userData: RegisterUserRetailRequest,
+  ): Promise<RegisterUserResponse> {
+    const { password, ...data } = userData;
+    const hashedPassword = await bcrypt.hash(password, this.hashRound);
+    return await this.usersRepo.createUser({
+      password: hashedPassword,
+      ...data,
+    });
+  }
+
+  async logIn(userData: LoginRequest): Promise<JWTToken> {
+    const user = await this.usersRepo.getUserByEmail(userData.email);
+    if (!user) {
+      throw new InvalidRequestError('Incorrect email. Please try again.');
+    }
+    const validUser = await bcrypt.compare(userData.password, user.password);
+    if (!validUser) {
+      throw new PermissionError('Incorrect password.');
+    }
+    return await this.generateToken({ userID: user.id });
+  }
 
   signAccessToken(payload: JWTPayload): string {
     return this.jwtService.sign(payload, this.jwtAccessTokenOptions);
@@ -36,12 +91,31 @@ export class AuthService {
   }
 
   async generateToken(jwtPayload: JWTPayload): Promise<JWTToken> {
-    const accessToken = this.signAccessTokenAsync(jwtPayload);
-    const refreshToken = this.signRefreshTokenAsync(jwtPayload);
-
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signAccessTokenAsync(jwtPayload),
+      this.signRefreshTokenAsync(jwtPayload),
+    ]);
+    // const accessToken = this.signAccessTokenAsync(jwtPayload);
+    // const refreshToken = this.signRefreshTokenAsync(jwtPayload);
+    if (backendConfig.node_env === 'development') {
+      await this.usersRepo.updateToken(jwtPayload.userID, accessToken);
+    }
     return {
-      accessToken: await accessToken,
-      refreshToken: await refreshToken,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     };
   }
+
+  async profile(userId: number): Promise<User> {
+    return await this.usersRepo.findUniqueUser({ userId: userId });
+  }
+
+  // async emailVerification(email){
+  //   var sent = await this.sendEmailVerification(email);
+  //     if(sent){
+  //       return new
+  //     } else {
+  //       return new
+  //     }
+  // }
 }
