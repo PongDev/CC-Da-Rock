2eea18edfd69b885c370cd7d14fd42766279ff12
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-07-22 21:23:46 +0700

    feat: add react-hook-form to package dependencies
    Adds the "react-hook-form" package for form handling in the application.

--- apps/frontend/src/components/FormInput.tsx
+++ apps/frontend/src/components/FormInput.tsx
@@ -10,9 +10,16 @@ export const FormInput = forwardRef<FormControlProps, "input">((props, ref) => {
   const { label, type, ...rest } = props;
 
   return (
-    <FormControl ref={ref} {...rest}>
+    <FormControl>
       <FormLabel fontWeight="bold">{label}</FormLabel>
-      <Input variant="filled" type={type} size="lg" shadow="lg" />
+      <Input
+        variant="filled"
+        type={type}
+        size="lg"
+        shadow="lg"
+        ref={ref}
+        {...rest}
+      />
     </FormControl>
   );
 });
