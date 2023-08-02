88cb70d4c8d3175537c18b504834aa1f26f91168
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-08-03 06:13:53 +0700

    feat: underconstruction page

---
+++ apps/frontend/src/pages/privilege.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header";
+import { UnderDev } from "@/components/Underdev";
+
+export default function Privilege() {
+  return (
+    <>
+      <Header />
+      <UnderDev />
+    </>
+  );
+}
