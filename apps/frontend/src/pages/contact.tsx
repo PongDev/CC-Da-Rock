88a5429d1105440a5bcea1cf1856196f9136920f
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-08-05 01:04:28 +0700

    feat: more immersive fake contact page

--- apps/frontend/src/pages/contact.tsx
+++ apps/frontend/src/pages/contact.tsx
@@ -12,15 +12,38 @@ import {
   Spacer,
   Stack,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export default function Contact() {
+  const { register, reset, handleSubmit } = useForm();
+
+  const toast = useToast();
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(data: any) {
+    console.log("Sending", data);
+
+    // fake delay for immersive experience
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setLoading(false);
+
+    toast({
+      title: "Message sent!",
+      colorScheme: "green",
+    });
+    reset();
+  }
+
   return (
     <>
       <Header />
 
       <Container maxW="container.lg" mt="10">
-        <Stack as="form" spacing={6}>
+        <Stack as="form" spacing={6} onSubmit={handleSubmit(onSubmit)}>
           <Box>
             <Heading>Contact Us</Heading>
             <Divider borderWidth={4} />
@@ -28,18 +51,34 @@ export default function Contact() {
 
           <Spacer />
 
-          <FormInput label="Name" type="text" required />
-          <FormInput label="Email" type="text" required />
+          <FormInput label="Name" type="text" required {...register("name")} />
+          <FormInput
+            label="Email"
+            type="email"
+            required
+            {...register("email")}
+          />
           {/* <FormInput label="Message" type="text" textare required /> */}
 
           <FormControl>
             <FormLabel fontWeight="bold">Message</FormLabel>
-            <Textarea variant="filled" shadow="lg" required />
+            <Textarea
+              variant="filled"
+              shadow="lg"
+              required
+              {...register("message")}
+            />
           </FormControl>
 
           <Spacer />
 
-          <Button colorScheme="green" color="black" size="lg" type="submit">
+          <Button
+            colorScheme="green"
+            color="black"
+            size="lg"
+            type="submit"
+            isLoading={loading}
+          >
             Send
           </Button>
         </Stack>
