83e1283b2c1c4c78a15cbea69edb73444d9b9d77
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-08-08 02:23:32 +0700

    fix: text is membership page too small

--- apps/frontend/src/pages/portfolio/membership.tsx
+++ apps/frontend/src/pages/portfolio/membership.tsx
@@ -1,6 +1,15 @@
 import { PortHead } from "@/components/portfolio/PortHead";
 import { Header } from "@/components/Header";
-import { Heading, Img, Progress, Stack, Text, VStack } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  Img,
+  Progress,
+  Spacer,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { Image } from "@chakra-ui/next-js";
 
 export default function Membership() {
@@ -12,7 +21,7 @@ export default function Membership() {
       <Header />
       <PortHead />
 
-      <Stack textAlign="center" mt={16}>
+      <Stack textAlign="center" my={16}>
         <Heading size="2xl">Solar Seed</Heading>
 
         <VStack>
@@ -22,11 +31,17 @@ export default function Membership() {
             width={192}
             height={192}
           />
-          <Text> {co2} kCO2e</Text>
+          <Spacer />
+          <HStack>
+            <Text fontSize="2xl" fontWeight="bold" color="green.600">
+              {co2}
+            </Text>
+            <Text>kCO2e</Text>
+          </HStack>
           <Heading>You Have Offset</Heading>
         </VStack>
 
-        <Progress value={50} size="lg" mx={4} rounded="full" />
+        <Progress value={co2} size="lg" mx={4} rounded="full" />
       </Stack>
     </>
   );
