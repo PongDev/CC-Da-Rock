8d98a4675a868a9d2989b6cefd5390ffbc527172
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-08-08 14:29:01 +0700

    fix: store page not responsive

--- apps/frontend/src/pages/store.tsx
+++ apps/frontend/src/pages/store.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { Link } from "@chakra-ui/next-js";
 import {
   Box,
+  Container,
   Grid,
   Heading,
   HStack,
@@ -23,12 +24,14 @@ const stores = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
     image: "https://picsum.photos/256",
+    disable: true,
   },
   {
     name: "store-2",
     displayName: "Store 2",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     image: "https://picsum.photos/256?blur=1",
+    disable: true,
   },
 ];
 
@@ -37,25 +40,31 @@ export default function Store() {
     <>
       <Header />
 
-      <Box px={8}>
+      <Box>
         <VStack gap={10}>
           <Heading size="3xl">Store</Heading>
           {stores.map((store, i) => (
             <Grid
               as={Link}
               key={i}
-              gap={8}
+              gap={{ base: 4, md: 8 }}
               gridTemplateColumns="30% 70%"
-              width="50%"
+              width={{ base: "100%", md: "80%", lg: "60%", xl: "50%" }}
               p={4}
               boxShadow="lg"
               transition="all 0.2s"
               _hover={{
                 transform: "scale(1.05)",
               }}
-              href={`/store/${store.name}`}
+              href={store.disable ? {} : `/store/${store.name}`}
+              opacity={store.disable ? 0.5 : 1}
+              cursor={store.disable ? "not-allowed" : "pointer"}
             >
-              <Img src={store.image} boxSize={128} objectFit="cover" />
+              <Img
+                src={store.image}
+                boxSize={{ base: 32, sm: 64, md: 128, lg: 192 }}
+                objectFit="cover"
+              />
               <Box>
                 <Heading>{store.displayName}</Heading>
                 <Text>{store.description}</Text>
